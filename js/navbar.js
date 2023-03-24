3b967c035691306213c1bf3274fe76c8cc0cb29d
Wesley Hoy <whoy@ey3.tech>
2023-03-24 15:28:08 -0400

    Improve navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 // navbar appearing after scroll
 const navbar = document.getElementById("navbar");
-const navbarScroll = 50; // how far scrolled should the page be before the navbar shows
+const navbarScroll = 10; // how far scrolled should the page be before the navbar shows
 window.addEventListener("scroll", (e) => {
     if (window.scrollY >= navbarScroll) {
         navbar.classList.remove("hidden");
