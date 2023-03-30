14267b9069c1bd35ef9037e19e07324593ab6e20
Gus Ruben <gusruben@protonmail.com>
2023-03-30 16:15:01 -0400

    Add navbar hamburger

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,15 @@
 // navbar appearing after scroll
-const navbar = document.getElementById("navbar");
+const navbarWrapper = document.getElementById("navbar-wrapper");
 const navbarScroll = 10; // how far scrolled should the page be before the navbar shows
 window.addEventListener("scroll", (e) => {
     if (window.scrollY >= navbarScroll) {
-        navbar.classList.remove("hidden");
+        navbarWrapper.classList.remove("hidden");
     } else {
-        navbar.classList.add("hidden");
+        navbarWrapper.classList.add("hidden");
     }
 });
 
+const navbarButton = document.getElementById("navbar-button-wrapper");
+navbarButton.addEventListener("click", () => {
+    window.innerWidth <= 500 && document.getElementById("navbar").classList.toggle("active");
+})
